3e914aa8a0788b0d42fed248e4ee4df5a8b6dde2
LidiaAlemu <lidiadanialemu@gmail.com>
2025-11-11 11:32:55 -0500

    feat: complete dependency installation and basic React setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Socket.io Chat Application</h1>
+      <p>React client is running!</p>
+    </div>
+  )
+}
+
+export default App
